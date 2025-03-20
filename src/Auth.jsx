7e2c64217411e375ac730d8f5efbf18f4da87c30
JuanAngelBi <105744962+JuanAngelBi@users.noreply.github.com>
2025-03-20 16:21:20 -0300

    Avance CSS
    :)

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -26,20 +26,17 @@ export default function Auth({ user, setUser, auth, provider }) {
         <div className="logo">
           <h1>MyBlog</h1>
         </div>
-        <a href="https://github.com/turepositorio" target="_blank" rel="noopener noreferrer">
-          <button><img src="https://img.icons8.com/ios_filled/512/FFFFFF/github.png" alt="Github logo" className="button-icon"/></button>
-        </a>
         <div className="auth-button">
           {user ? (
             <button onClick={handleSignOut}>Cerrar sesión</button>
           ) : (
-            <button onClick={handleSignIn}>Iniciar sesión con Google</button>
+            <button onClick={handleSignIn}>Iniciar sesión</button>
           )}
         </div>
       </header>
-      <main>
+      <main className="">
         <img
-          src="ruta-de-la-imagen-ia.jpg"
+          src="https://img.asmedia.epimg.net/resizer/v2/YZBVQL7EXNF5NESXYCNP2NMMEQ.jpg?auth=93c621f33452faa67e47d50a4803953522276428b7fbe71685fc23e0fbc836b3&width=1288&height=725&smart=true"
           alt="IA en películas"
           className="ia-image"
         />
